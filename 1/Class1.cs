826416fd26c06a8b91b01fa008a3d2a76f67986a
shalimo.dasha@bk.ru <shalimo.dasha@bk.ru>
2020-02-24 12:17:54 +0300

    Написание и объявление заданных переменных

---
+++ 1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rab1
+{
+    class produkt
+    {
+        private string name = new string(' ', 20);
+        int amount;
+        private float cost;
+        public produkt()
+        {
+            this.name = "Название";
+            this.amount = 0;
+            this.cost = 0;
+        }
+        public produkt(string name, int amount, float cost)
+        {
+            this.name = name;
+            this.amount = amount;
+            this.cost = cost;
+
+        }
+        
+        
+    }
+}
